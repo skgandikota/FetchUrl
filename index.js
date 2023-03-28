501fc98630cbd8f9c7f1fd87dbd58cb57e42f8af
skgandikota <sai.gandikota@marks-and-spencer.com>
2023-03-28 17:48:49 +0530

    modified response

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ try {
   const url = core.getInput("url");
   const method = core.getInput("method") || "GET";
   const headers = JSON.parse(core.getInput("headers")) || {};
-  const body = (method !== 'GET' && method !== 'HEAD') ? core.getInput("body") : null;
+  const body =
+    method !== "GET" && method !== "HEAD" ? core.getInput("body") : null;
 
   fetch(url, { method, headers, body })
     .then((response) => {
@@ -14,13 +15,15 @@ try {
       return response.json();
     })
     .then((body) => {
-      core.setOutput("body", body);
+      core.setOutput(
+        "body",
+        typeof body === "object" ? JSON.stringify(body, null, 2) : body
+      );
     })
     .catch((err) => {
       core.setOutput("status", err.status);
       core.setOutput("body", err.message);
     });
-
 } catch (error) {
   core.setFailed(error.message);
   console.log(error.message);
